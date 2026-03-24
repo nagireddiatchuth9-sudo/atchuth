3649452c61f317fd1849019bb6f86b61c73a4b07
nagireddiatchuth9-sudo <nagirediatchuth9@gmail.com>
2026-03-24 13:15:05 +0530

    day5

---
+++ day5.py
@@ -0,0 +1,60 @@
+'''string ---> '''
+
+'''
+any = 356
+ss = '586'
+for i in ss:
+    print(i)
+    
+'''    '''
+   
+string ---> string is a collaction of char, which represter
+by " " or '' we can access the using indexing and also
+scicing
+
+any = 'python programing'
+print (any[7:13])
+
+'''   
+''' 
+string ---> string is a collaction of char, which represter
+by " " or '' we can access the using indexing (string
+can also allow nagitive indexing)and also
+slicing. these is also immutable, where i could not adle to modifacation
+on the paticular vari... 
+any="i am atchuth"
+print(any[5:])
+so = any.replace("python","jav =a")
+print(any)
+print(so)''' ''' 
+
+
+
+''' '''
+a_day = "vassu from tantuvalasa, hava 4+ years of exp as data"
+print(len(a_day))
+''' 
+
+'''
+some = "python is a prograMMing langiage "
+print (some.lower())''''''
+
+some = "python is programming LANGUANE"
+print (some.lower())'''''' 
+
+''' '''
+some = "python is a programming languane"
+print (some.replace("programming","nrml"))
+
+'''
+some =" python is a programming language"
+if  "ythn" in some:
+    print ("yes")
+else:
+    print("no")
+
+
+
+
+
+                                               
