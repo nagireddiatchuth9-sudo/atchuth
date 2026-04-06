21fe1d3071fb37b38b0301c207cab6d1162d6693
nagireddiatchuth9-sudo <nagirediatchuth9@gmail.com>
2026-04-06 12:59:06 +0530

    day13

---
+++ day.13py.py
@@ -0,0 +1,75 @@
+'''Function()
+----------
+--> tise function a block of code which is reusable.
+--> two types 1.bulit-in or in-bulit
+            2.user define
+
+
+1.built-in or in-build
+---------------------------------
+-->they comes with program and those are already defined......
+eg..
+
+2.user define
+-------------
+-->this created by person who developing or using for developing
+
+note
+-----
+-->it's starts with def keywords followed by fune name
+--> and it has calling func...
+    you have to call the function for it to work
+eg : def func_name():# in the  bracked here put variable which  are called parameter
+     ........
+     func_name()# here inside the parameter they are calles agurment
+a = 5 
+def even (a) 
+    if a % 2==0:
+  else:
+      print("not even")
+
+
+prime number in  function
+
+prime_num = 7
+count = 0
+def prime_check(num,k):
+    for j in range(1,num+1):
+        if num % j == 0:
+            k += 1
+    if k == 2:
+        print("print")
+    else:
+        print("not")
+prime_check(prime_num,count)'''
+
+prime_num = 8
+count = 0
+def prime_check(num,k):
+    if i in range(2,num+2):
+        if num % i == 0:
+            k += 2
+    if k == 3:
+        print("print")
+    else:
+        print("not")
+print_check(prime_num,count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
